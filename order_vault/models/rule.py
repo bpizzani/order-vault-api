1517b8a862917ae9d3cfaf209fc949e5514b598f
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-02-08 20:47:14 +0100

    Update rule.py

--- order_vault/models/rule.py
+++ order_vault/models/rule.py
@@ -6,3 +6,5 @@ class Rule(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     attribute = db.Column(db.String(50), nullable=False)
     threshold = db.Column(db.Integer, nullable=False)
+    promocode = db.Column(db.String(100), nullable=True)
+
