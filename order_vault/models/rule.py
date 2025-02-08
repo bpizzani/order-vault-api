bb41692ffb832390e95f87b29fd3436318843615
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-02-08 15:11:27 +0100

    Update rule.py

--- order_vault/models/rule.py
+++ order_vault/models/rule.py
@@ -2,6 +2,7 @@ from order_vault.models.db import db
 from datetime import datetime
 
 class Rule(db.Model):
+    __tablename__ = 'rule'
     id = db.Column(db.Integer, primary_key=True)
     attribute = db.Column(db.String(50), nullable=False)
     threshold = db.Column(db.Integer, nullable=False)
