2f26c5251711b30dd2a832678e1fca404339d62e
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-02-08 14:53:25 +0100

    Update main.py

--- order_vault/main.py
+++ order_vault/main.py
@@ -20,7 +20,7 @@ CORS(app, supports_credentials=True)
 # Flask App Setup
 app.secret_key = "your_secret_key"
 # Database configuration
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e' #os.getenv('DATABASE_URL') #os.environ.get("DATABASE_URL") #"sqlite:///orders_v4.db" #os.environ.get("DATABASE_URL", "sqlite:///orders_v4.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e' #os.getenv('DATABASE_URL') #os.environ.get("DATABASE_URL") #"sqlite:///orders_v4.db" #os.environ.get("DATABASE_URL", "sqlite:///orders_v4.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 print(f"Database URI: {os.getenv('DATABASE_URL')}")
