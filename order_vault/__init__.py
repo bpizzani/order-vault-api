3a5a6c74aaeb50e9c816348d3859383a1c57c80b
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-05-24 17:20:57 +0200

    Update __init__.py

--- order_vault/__init__.py
+++ order_vault/__init__.py
@@ -1,26 +1,26 @@
-#from flask import Flask
-#from flask_cors import CORS
-
-#app = Flask(__name__)
-#CORS(app)  # This allows all domains to access your API
-
-#from order_vault import main
-
 from flask import Flask
-from .extensions import db, migrate, cors, init_neo4j
-from .routes import register_blueprints
+from flask_cors import CORS
 
-def create_app(config_object="config.DevConfig"):
-    app = Flask(__name__, static_folder="../static", template_folder="../templates")
-    app.config.from_object(config_object)
+app = Flask(__name__)
+CORS(app)  # This allows all domains to access your API
 
-    # initialize extensions
-    db.init_app(app)
-    migrate.init_app(app, db)
-    cors.init_app(app, supports_credentials=True)
-    init_neo4j(app)
+from order_vault import main
+
+#from flask import Flask
+#from .extensions import db, migrate, cors, init_neo4j
+#from .routes import register_blueprints
+
+#def create_app(config_object="config.DevConfig"):
+#    app = Flask(__name__, static_folder="../static", template_folder="../templates")
+#    app.config.from_object(config_object)
+
+ #   # initialize extensions
+ #   db.init_app(app)
+ #   migrate.init_app(app, db)
+ #   cors.init_app(app, supports_credentials=True)
+ #   init_neo4j(app)
 
     # register all route blueprints
-    register_blueprints(app)
+ #   register_blueprints(app)
 
-    return app
+ #   return app
