c77021d6954515966ec1e5aac242bc9b07c8b248
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-05-24 18:23:46 +0200

    Update __init__.py

--- order_vault/routes/__init__.py
+++ order_vault/routes/__init__.py
@@ -1,9 +1,9 @@
-from .fingerprint import bp as fingerprint_bp
-from .orders      import bp as orders_bp
-from .rules       import bp as rules_bp
-from .evaluate    import bp as evaluate_bp
-from .customer    import bp as customer_bp
-from .promocode   import bp as promocode_bp
+from .fingerprint import fingerprint_bp
+from .orders      import orders_bp
+from .rules       import rules_bp
+from .evaluate    import evaluate_bp
+from .customer    import customer_bp
+from .promocode   import promocode_bp
 
 def register_blueprints(app):
     for bp in (fingerprint_bp, orders_bp, rules_bp,
