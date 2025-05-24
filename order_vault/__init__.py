2a7c313dd6409e2d2053457470bd4421487a800d
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-05-24 17:40:31 +0200

    Update __init__.py

--- order_vault/__init__.py
+++ order_vault/__init__.py
@@ -5,21 +5,3 @@ app = Flask(__name__)
 CORS(app)  # This allows all domains to access your API
 
 from order_vault import main
-
-from .extensions import db, migrate, cors, init_neo4j
-from .routes import register_blueprints
-
-def create_app(config_object="config.DevConfig"):
-    app = Flask(__name__, static_folder="../static", template_folder="../templates")
-    app.config.from_object(config_object)
-
-    # initialize extensions
-    db.init_app(app)
-    migrate.init_app(app, db)
-    cors.init_app(app, supports_credentials=True)
-    init_neo4j(app)
-
-    # register all route blueprints
-    register_blueprints(app)
-
-    return app
