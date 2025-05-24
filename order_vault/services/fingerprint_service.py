4a6e9ce73a593c478c06cc56f91a71d7c881faca
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-05-24 16:56:58 +0200

    Create fingerprint_service.py

---
+++ order_vault/services/fingerprint_service.py
@@ -0,0 +1,4 @@
+def select_features(data):
+    keys = ["userAgent","platform","screenRes","colorDepth",
+            "timezone","languages","plugins","webGLFingerprint","canvasFingerprint"]
+    return [str(data.get(k, "")) for k in keys]
