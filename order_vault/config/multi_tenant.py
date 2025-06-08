664167b0394de023921a94338f3f3a2e14e00205
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-06-08 12:08:31 +0200

    Update multi_tenant.py

--- order_vault/config/multi_tenant.py
+++ order_vault/config/multi_tenant.py
@@ -1,10 +1,10 @@
 
 CLIENT_DATABASES = {
     "client_a": {
-        "postgres_uri": "postgresql://user:pass@host:port/db_client_a",
-        "neo4j_uri": "neo4j+s://host_a.neo4j.io",
+        "postgres_uri": "postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e",
+        "neo4j_uri": "neo4j+s://f34af65f.databases.neo4j.io",
         "neo4j_user": "neo4j",
-        "neo4j_password": "password_a"
+        "neo4j_password": "OPESlEPx3V4kYLSOo86X5fHX0k_HhKprCVG_erEfi7A"
     },
     "client_b": {
         "postgres_uri": "postgresql://user:pass@host:port/db_client_b",
@@ -13,3 +13,4 @@ CLIENT_DATABASES = {
         "neo4j_password": "password_b"
     },
 }
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e' #os.getenv('DATABASE_URL') #os.environ.get("DATABASE_URL") #"sqlite:///orders_v4.db" #os.environ.get("DATABASE_URL", "sqlite:///orders_v4.db")
