a14b4acb4017be47305b0a091b8cca5eb7ac25f2
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-06-08 13:09:25 +0200

    Update tenants.py

--- order_vault/settings/tenants.py
+++ order_vault/settings/tenants.py
@@ -1,4 +1,4 @@
-CLIENT_DATABASES = {
+TENANT_DATABASES = {
     "client_a": {
         "postgres_uri": "postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e",
         "neo4j_uri": "neo4j+s://f34af65f.databases.neo4j.io",
