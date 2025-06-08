9281a70f3127079382d0785f2b86d826fcd59fa9
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-06-08 18:27:16 +0200

    Update home.py

--- order_vault/routes/home.py
+++ order_vault/routes/home.py
@@ -1,22 +1,27 @@
 from flask import Blueprint, render_template
 from flask import request, session, jsonify
 from werkzeug.security import check_password_hash
+from utils.auth import login_required
 
 home_bp = Blueprint("home", __name__, url_prefix="/")
 
 @home_bp.route("", methods=["GET"])
+@login_required
 def home():
     return render_template("home2.html")
 
 @home_bp.route("/promotion_dashboard", methods=["GET"])
+@login_required
 def promotion_ui():
     return render_template("promotion_dashboard.html")
     
 @home_bp.route('/rules')
+@login_required
 def rules_ui():
     return render_template('rules.html')
 
 @home_bp.route("/island", methods=["GET"])
+@login_required
 def customer_ui():
     return render_template("island.html")
 
