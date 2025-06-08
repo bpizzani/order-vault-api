40dab68bb9e6bc7cb735566637cb15c01a1d9f48
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-06-08 12:30:04 +0200

    Create user.py

---
+++ order_vault/models/user.py
@@ -0,0 +1,9 @@
+from order_vault.models.db import db
+from datetime import datetime
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
+    client_id = db.Column(db.String, nullable=False)  # e.g., "nike", "zara"
