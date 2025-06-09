64ab9d17c926f1861ccd593ecac5ee4af3ad2d5c
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-06-09 03:01:41 +0200

    Update neo4j_service.py

--- order_vault/services/neo4j_service.py
+++ order_vault/services/neo4j_service.py
@@ -127,7 +127,7 @@ def create_graph(tx, G):
                 )
 
             # 3) Attribute → CONNECTED_TO → Attribute
-            elif node_label not in ['order', 'customer'] and neighbor_label not in ['order', 'customer']:
+            elif (node_label not in ['order', 'customer'] and neighbor_label not in ['order', 'customer'] and node_label != 'promocode' and neighbor_label != 'promocode'):
                 tx.run(
                     """
                     MATCH (a1:Attribute {type: $type1, value: $value1}),
@@ -144,6 +144,7 @@ def create_graph(tx, G):
     tx.run(
         """
         MATCH (c:Customer)-[:PLACED]->(o:Order)-[:HAS_ATTRIBUTE]->(a:Attribute)
+        WHERE a.type <> 'promocode'
         MERGE (c)-[:HAS_ATTRIBUTE]->(a)
         """
     )
