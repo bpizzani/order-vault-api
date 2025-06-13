0ef8890425c45bf5c1378afd022c1275d596f517
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-06-13 13:20:00 +0200

    Update routes.py

--- order_vault/auth/routes.py
+++ order_vault/auth/routes.py
@@ -42,10 +42,7 @@ def login():
 @auth_bp.route("/logout",methods=["GET", "POST"])
 def logout():
     session.clear()
-    return jsonify({
-        "message": "Logged out. Login again here:",
-        "login_url": "https://www.rediim.com/login"
-    }), 200
+    return render_template("logout.html"), 200
 
 @auth_bp.route("/create-user", methods=["GET"])
 def create_user_via_url():
