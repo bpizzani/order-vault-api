1111775a80bc077d2a41fa2f6673008586630ffb
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-06-16 23:35:31 +0200

    Update main.py

--- order_vault/main.py
+++ order_vault/main.py
@@ -11,7 +11,7 @@ from order_vault import app
 app.secret_key = "your_secret_key"
 app.config["SESSION_COOKIE_DOMAIN"] = ".rediim.com"
 app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
-app.config["SESSION_COOKIE_SECURE"] = True  # Only for HTTPS
+#app.config["SESSION_COOKIE_SECURE"] = True  # Only for HTTPS
 
 # ─── Shared PostgreSQL DB (Auth & User Table) ────
 app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e'
