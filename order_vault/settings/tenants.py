973557f9670c2c205591fe6d45a4ae58a9750a01
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-06-16 23:53:53 +0200

    Update tenants.py

--- order_vault/settings/tenants.py
+++ order_vault/settings/tenants.py
@@ -17,4 +17,10 @@ TENANT_DATABASES = {
         "neo4j_user": "neo4j",
         "neo4j_password": "OPESlEPx3V4kYLSOo86X5fHX0k_HhKprCVG_erEfi7A"
     },
+     "riccardo": {
+        "postgres_uri": "postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e",
+        "neo4j_uri": "neo4j+s://f34af65f.databases.neo4j.io",
+        "neo4j_user": "neo4j",
+        "neo4j_password": "OPESlEPx3V4kYLSOo86X5fHX0k_HhKprCVG_erEfi7A"
+    },
 }
