546d84cd974014e181621efd97e9469436f2d05b
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-07-20 19:57:43 +0200

    Update fingerprint.py

--- order_vault/models/fingerprint.py
+++ order_vault/models/fingerprint.py
@@ -5,8 +5,8 @@ class FingerprintEvents(db.Model):
     __tablename__ = 'fingerprint_events'
     id = db.Column(db.Integer, primary_key=True)
     client_id = db.Column(db.String(100), nullable=False)
-    user_id = db.Column(db.String(100), nullable=True)
-    visitor_id = db.Column(db.String(100), nullable=False)  # SHA256 = 64 hex chars
+    user_id = db.Column(db.String, nullable=True)
+    visitor_id = db.Column(db.String, nullable=False)  # SHA256 = 64 hex chars
     cookie_session = db.Column(db.String, nullable=True)
     local_storage_device = db.Column(db.String, nullable=True)
     user_agent =  db.Column(db.String, nullable=True)
