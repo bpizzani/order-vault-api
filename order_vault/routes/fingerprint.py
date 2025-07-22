75b598be3e6311d0a12231d25b8f1bccdc33ddcc
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-07-23 00:09:58 +0200

    Update fingerprint.py

--- order_vault/routes/fingerprint.py
+++ order_vault/routes/fingerprint.py
@@ -78,9 +78,9 @@ def fingerprint():
     vid = hashlib.sha256("|".join(features).encode()).hexdigest()
 
     # Store in DB
+    db_session = get_db_session_for_client(g.db_uri)
     save_fingerprint_event(db_session, g.client_id, data, vid)
     
-    #db_session = get_db_session_for_client(g.db_uri)
     #entry = FingerprintEvents(client_id=g.client_id, user_id=user_identifier_client, visitor_id=vid,js_visitor_id=fingerprint_js_visitor_id,tm_visitor_id=thumbmark_js_visitor_id, cookie_session=cookie_session,local_storage_device=user_identifier_device, user_agent=str(data.get('userAgent'))[0:50], webdriver=data.get('webdriver'), platform=platform)
     #db_session.add(entry)
     #db_session.commit()
