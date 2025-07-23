8bde5f0a87ab6ac0ec43392a2a6def7a8866d572
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-07-23 16:23:21 +0200

    Update fingerprint_es.js

--- order_vault/static/js/fingerprint_es.js
+++ order_vault/static/js/fingerprint_es.js
@@ -89,14 +89,6 @@ function getOrCreateUserId() {
     return uid;
 }
 
-function getOrCreateUserId() {
-    let uid = localStorage.getItem("user_id");
-    if (!uid) {
-        uid = crypto.randomUUID();
-        localStorage.setItem("user_id", uid);
-    }
-    return uid;
-}
 
 async function runFingerprintJs() {
     try {
