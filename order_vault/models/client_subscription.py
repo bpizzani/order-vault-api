e0be1ca26b90875a97f5b73c0e9661986ee23ec6
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-07-26 16:44:14 +0200

    Update client_subscription.py

--- order_vault/models/client_subscription.py
+++ order_vault/models/client_subscription.py
@@ -10,7 +10,7 @@ class ClientSubscription(db.Model):
     subscription_start = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     subscription_end = db.Column(db.DateTime, nullable=False)
     max_api_calls = db.Column(db.Integer, nullable=False)
-    current_api_calls = db.Column(db.Integer, nullable=True)
+    current_api_calls = db.Column(db.Integer, default=0, nullable=True)
 
     def __repr__(self):
         return f"<ClientSubscription client_id={self.client_id} start={self.subscription_start} end={self.subscription_end} max_api_calls={self.max_api_calls}>"
