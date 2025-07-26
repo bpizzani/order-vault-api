3d3d957c302188c3502db653de1146b85990e8c4
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-07-26 18:29:32 +0200

    Update evaluate.py

--- order_vault/routes/evaluate.py
+++ order_vault/routes/evaluate.py
@@ -2,9 +2,41 @@ from flask import Blueprint, request, jsonify, current_app, g
 from order_vault.models.rule import Rule
 from order_vault.services.neo4j_service import evaluate_attributes
 from order_vault.auth.api_auth import require_api_key   
+from order_vault.utils.db_session import get_db_session_for_client 
+from order_vault.models.evaluation import Evaluation  
 
 evaluate_bp = Blueprint("evaluate", __name__, url_prefix="/api")
 
+ 
+def async_save_evaluation_event(db_uri, client_id, user_identifier_client, data, visitor_id):
+    # Create a new DB session in the background thread
+    session = get_db_session_for_client(db_uri)
+    try:
+        save_evaluation_event(session, client_id, user_identifier_client, data, visitor_id)
+    except Exception as e:
+        print("Error in async DB save:", e)
+    finally:
+        session.close()
+        
+def save_evaluation_event(db_session, client_id, user_id=0, checkout_id=0, order_id=0, session_id=0, values , risk_decision):
+    entry = FingerprintEvents(
+        client_id = client_id,
+        user_id = user_id,
+        checkout_id = checkout_id,
+        order_id = order_id,
+        session_id = session_id,
+        promocode = promo,
+        promotion_id = promo,
+        visitor_id = values["device_id"]
+        local_storage_device = alues["local_session_id"]
+        risk_decision = risk_decision["overall_abusive"]
+        risk_features = risk_decision["evaluation_results"]
+        created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    )
+    db_session.add(entry)
+    db_session.commit()
+    print("Fingerprint Event Saved")
+    
 @evaluate_bp.route("/evaluate", methods=["GET"])
 @require_api_key
 def evaluate():
@@ -13,9 +45,11 @@ def evaluate():
     types = request.args.get("attribute_types", "device_id").split(",")
     promo = request.args.get("promocode")
     values = {t: request.args.get(t) for t in accepted_types if request.args.get(t)}
+
     checkout_id = request.args.get("checkout_id","")
     user_id = request.args.get("user_id","")
-
+    session_id = request.args.get("session_id","")
+    order_id = request.args.get("order_id","")
     
     import time
     start = time.time()
@@ -51,6 +85,15 @@ def evaluate():
         overall = overall or abusive
 
     print("Evaluate total time:", time.time() - start)
+
+    # Fire off background thread to save
+    Thread(
+        target=async_save_fingerprint_event,
+        args=(g.db_uri, g.client_id, user_identifier_client, data, vid),
+        daemon=True
+    ).start()
+
+    risk_decision = {"evaluation_results": final, "overall_abusive": overall}
 
     return jsonify({"evaluation_results": final, "overall_abusive": overall}), 200
 
