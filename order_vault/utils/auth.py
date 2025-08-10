2495ba8055bfef9fdca686bb53128259054533c0
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-08-10 14:19:13 +0200

    Update auth.py

--- order_vault/utils/auth.py
+++ order_vault/utils/auth.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from flask import session, redirect, url_for, g
 from order_vault.models.user import User
+from order_vault.models.client_subscription import ClientSubscription
 from order_vault.settings.tenants import TENANT_DATABASES
 from neo4j import GraphDatabase
 
@@ -18,9 +19,12 @@ def login_required(f):
         tenant = TENANT_DATABASES.get(user.client_id)
         if not tenant:
             return "Invalid tenant", 403
+            
+        subscription = ClientSubscription.query.filter_by(client_id=user.client_id).first()
 
         g.user = user
         g.client_id = user.client_id
+        g.subscription_type = subscription.type
         g.db_uri = tenant["postgres_uri"]
         g.neo4j_driver = GraphDatabase.driver(
             tenant["neo4j_uri"],
