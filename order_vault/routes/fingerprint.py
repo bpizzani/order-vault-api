f112f25c8307a0e7f2653bf063163893b0235aca
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-08-10 13:55:42 +0200

    Update fingerprint.py

--- order_vault/routes/fingerprint.py
+++ order_vault/routes/fingerprint.py
@@ -143,7 +143,8 @@ def fingerprint():
     print(f"User Agent: {data.get('userAgent') }")
     print(f"webdriver: {data.get('webdriver') }")
 
-    
+    print(f"Promocode: {data.get('promocode') }")
+
     cookie_session = data.get("sessionId")
     print(f"cookie_session detected: {cookie_session}")
     
