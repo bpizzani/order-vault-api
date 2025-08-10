129f90a4254a947fc41ce62e97f69b9d3a4578c0
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-08-10 13:22:24 +0200

    Update api_Template_web_js.js

--- order_vault/utils/api_Template_web_js.js
+++ order_vault/utils/api_Template_web_js.js
@@ -1,4 +1,24 @@
-    	    
+
+  -- get Fingerprint
+
+    let rediim_fingerprint = null;
+    async function runInHouseFingerprint(api_key,client_id, user_id = 0) {
+            try {
+                const InHouseFingerprint = await import('https://api.rediim.com/static/js/fingerprint_web.js');
+                const { visitorId, localSessionId } = await InHouseFingerprint.sendFingerprint(api_key, client_id,user_id);
+		rediim_fingerprint = visitorId
+				
+		localStorage.setItem("rediim_fingerprint", visitorId);
+        localStorage.setItem("local_session_id", localSessionId);
+
+            } catch (error) {
+                console.error("Error getting InHouseFingerprint:", error);
+            }
+    }
+
+
+---- JS
+
 	async function finalizeOrderFrontend(orderData, api_key, client_id ) {
 	    try {
 	        const response = await fetch("https://api.rediim.com/finalize-order", {
@@ -28,16 +48,17 @@
 	        const clientUrl = "https://api.rediim.com/api/evaluate";
 		    
 	        try {
-	            const response = await fetch(`${clientUrl}?${params.toString()}`, {
-	                method: "GET",
+	            const response = await fetch("https://api.rediim.com/api/evaluate", {
+	                method: "POST",
 	                headers: {
 	                    "X-API-KEY": apiKey,
-			    "X-CLIENT-ID":client_id,
-	                }
+			             "X-CLIENT-ID":client_id,
+	                },
+                    body: JSON.stringify(params)
 	            });
 	
 	            const data = await response.json();
-	
+	            
 	            if (!response.ok) {
 	                throw new Error(data.error || "Evaluation failed");
 	            }
@@ -55,115 +76,12 @@
 	            console.error("Evaluation error:", err);
 	            alert("An error occurred while evaluating risk.");
 	        }
-	}	  
+	}	   
 
-    async function evaluateUserRisk(apiKey, client_id) {
-	    
-        const clientUrl = "https://api.rediim.com/api/evaluate";
-    
-	function generateRandomId(prefix = "") {
-	    return prefix + Math.random().toString(36).substring(2, 10);
-	}
-		
-	const user_id = localStorage.getItem("user_id");
-	const device_id = localStorage.getItem("rediim_fingerprint");
-	const local_session_id = localStorage.getItem("local_session_id");
 
-	const promocode = `promo_${Math.floor(Math.random() * 1000)}`;
-	const email = `user${Math.floor(Math.random() * 10000)}@example.com`;
-	const phone = `555${Math.floor(1000000 + Math.random() * 9000000)}`;
-	const session_id = local_session_id;
-	const checkout_id = generateRandomId("chk_");
-	const order_id = generateRandomId("ord_");
-	const card_details = `4444${Math.floor(1000 + Math.random() * 9000)}`;
-	
-	// Params for evaluation API
-	const params = new URLSearchParams({
-	promocode,
-	device_id,
-	email,
-	phone,
-	card_details,
-	session_id,
-	local_session_id,
-	user_id,
-	checkout_id,
-	order_id,
-	attribute_types: "device_id,phone,card_details,email,local_session_id,checkout_id,user_id,order_id,session_id"
-	});
+window.onload = async function() {
 
-	// Build orderData for finalize-order API
-	const orderData = {
-	id: order_id, // or use checkout_id depending on your backend
-	user_id,
-	name: `User ${user_id}`, // optional mock
-	email,
-	phone,
-	card_details,
-	promocode,
-	device_id,
-	local_session_id
-	};
-
-        try {
-            const response = await fetch(`${clientUrl}?${params.toString()}`, {
-                method: "GET",
-                headers: {
-                    "X-API-KEY": apiKey,
-		    "X-CLIENT-ID":client_id,
-                }
-            });
-
-            const data = await response.json();
-
-            if (!response.ok) {
-                throw new Error(data.error || "Evaluation failed");
-            }
-
-            console.log("Risk Evaluation Results:", data);
-
-            if (data.overall_abusive) {
-                alert("⚠️ Risk detected! This user may be abusing the promocode.");
-            } else {
-                alert("✅ User is clean.");
-		finalizeOrderFrontend(orderData, apiKey, client_id);
-            }
-
-        } catch (err) {
-            console.error("Evaluation error:", err);
-            alert("An error occurred while evaluating risk.");
-        }
-    }
-            
-function getUserId() {
-    let uid = localStorage.getItem("user_id");
-    if (!uid) {
-	uid = Math.floor(Math.random() * 1000) + 1; // Random number between 1 and 1000
-	localStorage.setItem("user_id", uid);
-    }
-    return uid;
-}
-  
-
-    async function runInHouseFingerprint(api_key,client_id, user_id = 0) {
-            try {
-                const InHouseFingerprint = await import('https://api.rediim.com/static/js/fingerprint_web.js');
-                const { visitorId, localSessionId } = await InHouseFingerprint.sendFingerprint(api_key, client_id,user_id);
-		rediim_fingerprint = visitorId
-                document.getElementById("fingerprint_inhouse").value = visitorId;
-		localStorage.setItem("rediim_fingerprint", visitorId);
-                localStorage.setItem("local_session_id", localSessionId);
-
-            } catch (error) {
-                console.error("Error getting InHouseFingerprint:", error);
-            }
-    }
-
-    let rediim_fingerprint = null;
-
-    window.onload = async function() {
-
-        const user_id = getUserId();
+        const user_id = getUserId(); -- Optional, but useful to have
         const key_api_fingerprint = "trial_abc";
         const client_id_fingerprint = "client_1";
 	    
@@ -173,15 +91,9 @@ function getUserId() {
         const apiKey = "abcde";
         const client_id = "client_c";
 	    
-        await evaluateUserRisk(apiKey, client_id);
-	    
-	//finalizeOrderFrontend(orderData);
-
-        
     };
 
 
-// api call 
     const form = document.getElementById('checkout-form');
     form.addEventListener('submit', async (e) => {
 	    
@@ -212,9 +124,8 @@ function getUserId() {
 	const checkout_id = generateRandomId("chk_");
 	const order_id = generateRandomId("ord_");
 	const card_details = form.card.value;
-	
-	// Params for evaluation API
-	const params = new URLSearchParams({
+
+	const params = {
 	promocode,
 	device_id,
 	email,
@@ -225,8 +136,8 @@ function getUserId() {
 	user_id,
 	checkout_id,
 	order_id,
-	attribute_types: "device_id,phone,card_details,email,local_session_id,checkout_id,user_id,order_id,session_id"
-	});
+	attribute_types: "device_id,phone,card_details,email,local_session_id"
+	};
 
 	// Build orderData for finalize-order API
 	const orderData = {
@@ -240,39 +151,11 @@ function getUserId() {
 	device_id,
 	local_session_id
 	};
-        const apiKey = "abcde";
-        const client_id = "client_c";
+	const apiKey = "abcde";
+	const client_id = "client_c";
 	    
 	evaluateUserRiskApi(params, orderData, apiKey, client_id);
-	    
-      //const response = await fetch('https://order-vault-client-webapp-13ee822f0ba9.herokuapp.com/checkout', {
-      //  method: 'POST',
-      //  headers: {
-      //    'Content-Type': 'application/json'
-      //  },
-      //  body: JSON.stringify(data)
-      //});
-
-      //const result = await response.json();
-      //document.getElementById('response').textContent = JSON.stringify(result, null, 2);
     });
+  </script>
 
 
-
-
-
-  
-    let rediim_fingerprint = null;
-    async function runInHouseFingerprint(api_key,client_id, user_id = 0) {
-            try {
-                const InHouseFingerprint = await import('https://api.rediim.com/static/js/fingerprint_web.js');
-                const { visitorId, localSessionId } = await InHouseFingerprint.sendFingerprint(api_key, client_id,user_id);
-		rediim_fingerprint = visitorId
-				
-		localStorage.setItem("rediim_fingerprint", visitorId);
-        localStorage.setItem("local_session_id", localSessionId);
-
-            } catch (error) {
-                console.error("Error getting InHouseFingerprint:", error);
-            }
-    }
