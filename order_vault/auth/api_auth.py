e52c86e44318e20ec678340ce9834314cb255816
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-09-07 13:55:05 +0200

    Update api_auth.py

--- order_vault/auth/api_auth.py
+++ order_vault/auth/api_auth.py
@@ -2,6 +2,8 @@ from functools import wraps
 from flask import request, g, jsonify
 from order_vault.models.user import User
 from order_vault.settings.tenants import TENANT_DATABASES
+from order_vault.models.tenant import Tenant
+from order_vault.utils.crypto import enc, dec
 from neo4j import GraphDatabase
 
 def require_api_key(func):
@@ -16,17 +18,24 @@ def require_api_key(func):
         if not user:
             return jsonify({"error": "Invalid API key"}), 401
 
-        tenant = TENANT_DATABASES.get(user.client_id)
+        
+        #tenant = TENANT_DATABASES.get(user.client_id)
+        tenant = Tenant.query.filter_by(client_id=user.client_id).first()
         if not tenant:
             return jsonify({"error": "Unknown tenant"}), 401
+            
+        pg_uri_dec = dec(tenant.pg_uri_enc)
+        neo4j_uri_dec = dec(tenant.neo4j_uri_enc)
+        neo4j_user_dec = dec(tenant.neo4j_user_enc)
+        neo4j_pass_dec = dec(tenant.neo4j_pass_enc)
 
         # Inject into `g`
         g.user = user
         g.client_id = user.client_id
-        g.db_uri = tenant["postgres_uri"]
+        g.db_uri = pg_uri_dec
         g.neo4j_driver = GraphDatabase.driver(
-            tenant["neo4j_uri"],
-            auth=(tenant["neo4j_user"], tenant["neo4j_password"])
+            neo4j_uri_dec,
+            auth=(neo4j_user_dec, neo4j_pass_dec)
         )
 
         return func(*args, **kwargs)
@@ -49,17 +58,23 @@ def require_api_key_fingerprint(func):
         if not user:
             return jsonify({"error": "Invalid API key or client ID"}), 401
 
-        tenant = TENANT_DATABASES.get(user.client_id)
+        #tenant = TENANT_DATABASES.get(user.client_id)
+        tenant = Tenant.query.filter_by(client_id=user.client_id).first()
         if not tenant:
             return jsonify({"error": "Unknown tenant"}), 401
 
-        #g.fingerprint_user_identifier_client = fingerprint_user_identifier_client
+        pg_uri_dec = dec(tenant.pg_uri_enc)
+        neo4j_uri_dec = dec(tenant.neo4j_uri_enc)
+        neo4j_user_dec = dec(tenant.neo4j_user_enc)
+        neo4j_pass_dec = dec(tenant.neo4j_pass_enc)
+
+        # Inject into `g`
         g.user = user
         g.client_id = user.client_id
-        g.db_uri = tenant["postgres_uri"]
+        g.db_uri = pg_uri_dec
         g.neo4j_driver = GraphDatabase.driver(
-            tenant["neo4j_uri"],
-            auth=(tenant["neo4j_user"], tenant["neo4j_password"])
+            neo4j_uri_dec,
+            auth=(neo4j_user_dec, neo4j_pass_dec)
         )
 
         return func(*args, **kwargs)
