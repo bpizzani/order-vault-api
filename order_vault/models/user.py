dafc17366dbf936849ee344e6b3f8b61410bb625
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-09-11 19:42:47 +0200

    Update user.py

--- order_vault/models/user.py
+++ order_vault/models/user.py
@@ -6,5 +6,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True, nullable=False)
     password_hash = db.Column(db.String, nullable=False)
-    client_id = db.Column(db.String, nullable=False)  # e.g., "nike", "zara"
+    client_id = db.Column(db.String, nullable=False)
     api_key = db.Column(db.String(128), unique=False, nullable=True)
+    pk_key = db.Column(db.String(128), unique=False, nullable=True)
+    pk_origin = db.Column(JSON, nullable=True)
+    jwt_secrets = db.Column(db.String(128), unique=False, nullable=True)
