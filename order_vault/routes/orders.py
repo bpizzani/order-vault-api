e7b6bd582c468572d9f77a2e9e9ecdc12a6e5e4c
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2025-09-11 19:43:52 +0200

    Update orders.py

--- order_vault/routes/orders.py
+++ order_vault/routes/orders.py
@@ -4,11 +4,13 @@ import threading
 from order_vault.services.neo4j_service import trigger_process_and_update
 from order_vault.main import app   # import your Flask app
 from order_vault.auth.api_auth import require_api_key 
+from order_vault.auth.bearer import require_auth
 
 orders_bp = Blueprint("orders", __name__)
 
 @orders_bp.route("/finalize-order", methods=["POST"])
-@require_api_key
+#@require_api_key
+@require_auth
 def finalize_order():
     order_data = request.get_json(force=True)
     order_data["created_at"] = datetime.utcnow().isoformat()
