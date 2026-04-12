97e06d8019113a781d9534209f1433677c2ab1c1
Bruno Pizzani <48102928+bpizzani@users.noreply.github.com>
2026-04-12 12:08:26 +0200

    Update tenants.py

--- order_vault/settings/tenants.py
+++ order_vault/settings/tenants.py
@@ -1,63 +1,63 @@
 TENANT_DATABASES = {
     "client_a": {
         "postgres_uri": "postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e",
-        "neo4j_uri": "neo4j+s://f34af65f.databases.neo4j.io",
+        "neo4j_uri": "neo4j+s://a7486838.databases.neo4j.io",
         "neo4j_user": "neo4j",
-        "neo4j_password": "OPESlEPx3V4kYLSOo86X5fHX0k_HhKprCVG_erEfi7A"
+        "neo4j_password": "HD9pqS30cHJ3CFcQyok3CIO6RlTigsoa_Nr0JJ3dgG8"
     },
     "client_c": {
         "postgres_uri": "postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e",
-        "neo4j_uri": "neo4j+s://f34af65f.databases.neo4j.io",
+        "neo4j_uri": "neo4j+s://a7486838.databases.neo4j.io",
         "neo4j_user": "neo4j",
-        "neo4j_password": "OPESlEPx3V4kYLSOo86X5fHX0k_HhKprCVG_erEfi7A"
+        "neo4j_password": "HD9pqS30cHJ3CFcQyok3CIO6RlTigsoa_Nr0JJ3dgG8"
     },
      "admin92": {
         "postgres_uri": "postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e",
-        "neo4j_uri": "neo4j+s://f34af65f.databases.neo4j.io",
+        "neo4j_uri": "neo4j+s://a7486838.databases.neo4j.io",
         "neo4j_user": "neo4j",
-        "neo4j_password": "OPESlEPx3V4kYLSOo86X5fHX0k_HhKprCVG_erEfi7A"
+        "neo4j_password": "HD9pqS30cHJ3CFcQyok3CIO6RlTigsoa_Nr0JJ3dgG8"
     },
     "admin": {
         "postgres_uri": "postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e",
-        "neo4j_uri": "neo4j+s://f34af65f.databases.neo4j.io",
+        "neo4j_uri": "neo4j+s://a7486838.databases.neo4j.io",
         "neo4j_user": "neo4j",
-        "neo4j_password": "OPESlEPx3V4kYLSOo86X5fHX0k_HhKprCVG_erEfi7A"
+        "neo4j_password": "HD9pqS30cHJ3CFcQyok3CIO6RlTigsoa_Nr0JJ3dgG8"
     },
 
     "client_1": {
         "postgres_uri": "postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e",
-        "neo4j_uri": "neo4j+s://f34af65f.databases.neo4j.io",
+        "neo4j_uri": "neo4j+s://a7486838.databases.neo4j.io",
         "neo4j_user": "neo4j",
-        "neo4j_password": "OPESlEPx3V4kYLSOo86X5fHX0k_HhKprCVG_erEfi7A"
+        "neo4j_password": "HD9pqS30cHJ3CFcQyok3CIO6RlTigsoa_Nr0JJ3dgG8"
     },
     "fpizzani88": {
         "postgres_uri": "postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e",
-        "neo4j_uri": "neo4j+s://f34af65f.databases.neo4j.io",
+        "neo4j_uri": "neo4j+s://a7486838.databases.neo4j.io",
         "neo4j_user": "neo4j",
-        "neo4j_password": "OPESlEPx3V4kYLSOo86X5fHX0k_HhKprCVG_erEfi7A"
+        "neo4j_password": "HD9pqS30cHJ3CFcQyok3CIO6RlTigsoa_Nr0JJ3dgG8"
     },
     "rediim_home_page": {
         "postgres_uri": "postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e",
-        "neo4j_uri": "neo4j+s://f34af65f.databases.neo4j.io",
+        "neo4j_uri": "neo4j+s://a7486838.databases.neo4j.io",
         "neo4j_user": "neo4j",
-        "neo4j_password": "OPESlEPx3V4kYLSOo86X5fHX0k_HhKprCVG_erEfi7A"
+        "neo4j_password": "HD9pqS30cHJ3CFcQyok3CIO6RlTigsoa_Nr0JJ3dgG8"
     },
     "riccardo84": {
         "postgres_uri": "postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e",
-        "neo4j_uri": "neo4j+s://f34af65f.databases.neo4j.io",
+        "neo4j_uri": "neo4j+s://a7486838.databases.neo4j.io",
         "neo4j_user": "neo4j",
-        "neo4j_password": "OPESlEPx3V4kYLSOo86X5fHX0k_HhKprCVG_erEfi7A"
+        "neo4j_password": "HD9pqS30cHJ3CFcQyok3CIO6RlTigsoa_Nr0JJ3dgG8"
     },
     "meeder": {
         "postgres_uri": "postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e",
-        "neo4j_uri": "neo4j+s://f34af65f.databases.neo4j.io",
+        "neo4j_uri": "neo4j+s://a7486838.databases.neo4j.io",
         "neo4j_user": "neo4j",
-        "neo4j_password": "OPESlEPx3V4kYLSOo86X5fHX0k_HhKprCVG_erEfi7A"
+        "neo4j_password": "HD9pqS30cHJ3CFcQyok3CIO6RlTigsoa_Nr0JJ3dgG8"
     },
     "admin_rediim": {
         "postgres_uri": "postgresql://u32cgla1pp9fm7:p6f656fa0f2edb9dda1653485f118f3b8379d957dce3469ef41d13f34d73e8cb1@c5flugvup2318r.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dc0evnfhnut69e",
-        "neo4j_uri": "neo4j+s://f34af65f.databases.neo4j.io",
+        "neo4j_uri": "neo4j+s://a7486838.databases.neo4j.io",
         "neo4j_user": "neo4j",
-        "neo4j_password": "OPESlEPx3V4kYLSOo86X5fHX0k_HhKprCVG_erEfi7A"
+        "neo4j_password": "HD9pqS30cHJ3CFcQyok3CIO6RlTigsoa_Nr0JJ3dgG8"
     },
 }
